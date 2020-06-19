5977792f12b0dcbe726b6fe9349e74b8442cfcf8
EvaJau <evaj@posteo.de>
2020-06-19 23:07:46 +0200

    vars and funcs

---
+++ explanations.js
@@ -0,0 +1,28 @@
+// variables
+
+var favColor = "red";
+var favColors = ["blue", "red"];
+var number = 1;
+var boolean = true;
+
+var richObject = {first: "yeah", second: "oh", third: 0};
+
+var third = richObject.third;
+
+// functions
+
+var whatIsMyFavColor = function () {
+    return "purple";
+}
+
+var doSomething = function () {
+    console.log("Do something!");
+}
+
+var area = function (x,y) {
+    return x*y;
+}
+
+
+
+
